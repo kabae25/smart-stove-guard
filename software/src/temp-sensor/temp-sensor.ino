6f16f4c5cc61bf0fe84c92e7cdc34efdffee6a15
Evan <evk0520@gmail.com>
2025-04-23 21:03:59 -0400

    minor changes before continuing layout tonight

--- software/src/temp-sensor/temp-sensor.ino
+++ software/src/temp-sensor/temp-sensor.ino
@@ -1,9 +1,19 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Wire.h>
+#include <Adafruit_MLX90614.h>
 
+Adafruit_MLX90614 mlx = Adafruit_MLX90614();
+
+void setup() {
+ Serial.begin(9600);
+ Serial.println("Adafruit MLX90614 test");  
+ mlx.begin();   
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  Serial.print("Ambient = "); Serial.print(mlx.readAmbientTempC());
+  Serial.print("*C\tObject = "); Serial.print(mlx.readObjectTempC()); Serial.println("*C");
+  Serial.print("Ambient = "); Serial.print(mlx.readAmbientTempF()); 
+  Serial.print("*F\tObject = "); Serial.print(mlx.readObjectTempF()); Serial.println("*F");
+  Serial.println();
+  delay(500);
 }
